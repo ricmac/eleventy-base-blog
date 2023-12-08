9fc4731f6b3bf6db14d6ff105f1d5f4ea50a9ac5
Richard MacManus <ricmac@users.noreply.github.com>
2023-12-08 12:17:15 +0000

    funky

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,7 +15,7 @@ module.exports = function(eleventyConfig) {
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
-		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
+		"./node_modules/prismjs/themes/prism-funky.css": "/css/prism-funky.css"
 	});
 
 	// Run Eleventy when these files change:
